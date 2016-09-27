1da389df187302189ac2596f89745e006d24bf86
Maciej Gawel <m.gawel@hotmail.com>
2016-09-27 22:12:39 +0200

    Frontend basic structure

---
+++ web/static/js/views/registrations/new.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RegistrationsNew extends React.Component {
+  render() {
+    return (
+      <div className="view-container registrations new">
+        <main>
+          hey
+        </main>
+      </div>
+    );
+  }
+}
+
+export default RegistrationsNew;
