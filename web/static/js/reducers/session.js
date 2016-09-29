7a8f834f37e4379ec4c74f925db85cbd63aea4a5
Maciej Gawel <m.gawel@hotmail.com>
2016-09-29 21:28:38 +0200

    Front-end authentication with React and Redux

--- web/static/js/reducers/session.js
+++ web/static/js/reducers/session.js
@@ -2,14 +2,20 @@ import Constants from '../constants';
 
 const initialState = {
   currentUser: null,
-  socket: null,
   error: null
 };
 
 export default function reducer(state = initialState, action = {} ) {
   switch (action.type) {
     case Constants.CURRENT_USER:
-      return { ...state, currentUser: action.currentUser };
+      return { ...state, currentUser: action.currentUser, errro: null };
+
+    case Constants.SESSIONS_ERROR:
+      return { ..state, error: action.error };
+
+    case Constants.USER_SIGNED_OUT:
+      return initialState;
+
     default:
       return state;
   }
